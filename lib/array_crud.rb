8710de09d1f52ca241db7e2df1b50df7b4279483
Oneil Williams <oneilswilliams@gmail.com>
2019-08-13 23:37:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,6 @@ end
 def add_element_to_end_of_array(array, element)
   colors = ["red", "blue", "green", "yellow"]
   colors.push("arrays!")
-  
 end 
 
 def add_element_to_start_of_array(array, element)
